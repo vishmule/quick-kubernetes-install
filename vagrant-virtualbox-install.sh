5efc32e215a91af86c63eadd831dcb82f40eac84
Vishwanath Mule <vishmule@outlook.com>
2020-06-07 00:04:38 +0530

    Create vagrant-virtualbox-install.sh

---
+++ vagrant-virtualbox-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author : vishmule
+# Install VirtualBox and Vagrant on Linux Host machine
+
+## Install following pre-requisites required for VirtualBox Installation
+
+yum install compat-libstdc++-33 SDL wget
+yum install gcc kernel-devel make libGL qt qt-devel libXmu
+
+## Download and Install VirtualBox
+
+wget https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0-6.0.4_128413_el7-1.x86_64.rpm
+rpm -ivh VirtualBox-6.0-6.0.4_128413_el7-1.x86_64.rpm
+
+## Download and Install Vagrant
+
+wget https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm
+rpm -ivh vagrant_2.2.4_x86_64.rpm
